f0b26376bb0d465636f5a53c64dc8a3e8887ea83
Phillip Johnson <philliptjohnson@hotmail.com>
2020-12-20 22:40:12 +0000

    Added pen domain model

---
+++ FountainOfKnowledge/Fountain.Domain/Models/Pen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fountain.Domain.Models
+{
+    public class Pen
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
