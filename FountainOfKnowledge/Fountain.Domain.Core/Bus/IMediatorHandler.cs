26296bb1eb06875da6b78457c65e041feae06607
Phillip Johnson <philliptjohnson@hotmail.com>
2020-12-23 16:27:07 +0000

    Added Domain.Core and implementation of in memory bus

---
+++ FountainOfKnowledge/Fountain.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,13 @@
+﻿using Fountain.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fountain.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
---
+++ FountainOfKnowledge/Fountain.Infrastructure.Bus/InMemoryBus.cs
@@ -0,0 +1,25 @@
+﻿using Fountain.Domain.Core.Bus;
+using Fountain.Domain.Core.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fountain.Infrastructure.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+    }
+}
