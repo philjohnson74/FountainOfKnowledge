0174613fb9bc6754f5b0a807ac6f608ca955deb4
Phillip Johnson <philliptjohnson@hotmail.com>
2020-12-27 10:14:28 +0000

    Added fountain.api project

---
+++ FountainOfKnowledge/Fountain.API/Controllers/PenController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fountain.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PenController : ControllerBase
+    {
+    }
+}
